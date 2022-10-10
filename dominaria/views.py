925c25067bf872962b9195a17f1d6a589b3b78a4
Aleix <al.dmc31@gmail.com>
2022-10-11 01:17:58 +0200

    Fix: per_games_won in views.py

--- dominaria/views.py
+++ dominaria/views.py
@@ -23,21 +23,21 @@ def about(request):
         def __repr__(self):
             return repr((self.player, self.points, self.per_games_won, self.rounds_played))
     for player in players:
-        rounds_won = Matchup.objects.filter(winner=Player.objects.filter(name=player.name)[0].id)
+        rounds_played = Matchup.objects.filter(
+            Q(player1=Player.objects.filter(name=player.name)[0].id) | Q(player2=Player.objects.filter(name=player.name)[0].id))
+        rounds_won = rounds_played.filter(winner=Player.objects.filter(name=player.name)[0].id)
         games_won = 0
         gl = 0
         per_games_won = 0
-        for _round in rounds_won:
-            if _round.player1 == _round.winner:
+        for _round in rounds_played:
+            if _round.player1 == player:
                 games_won += _round.games_won_by_p1
                 gl += _round.games_won_by_p2
-            elif _round.player2 == _round.winner:
+            elif _round.player2 == player:
                 games_won+=_round.games_won_by_p2
                 gl += _round.games_won_by_p1
         if games_won > 0:
             per_games_won = round(games_won/(games_won+gl)*100)
-        rounds_played = Matchup.objects.filter(
-            Q(player1=Player.objects.filter(name=player.name)[0].id) | Q(player2=Player.objects.filter(name=player.name)[0].id))
         draws = rounds_played.filter(winner=None).count()
         points = rounds_won.count() * 3 + draws
         classification.append(classificate(player.name, points, per_games_won, rounds_played.count()))
