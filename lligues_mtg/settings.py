4b4ecd6501b2f4d0e33184cb3de1896a150f8974
Aleix D <dominguez.aleix@apfutura.com>
2024-09-24 18:06:20 +0200

    remove heroku from sttings.py

--- lligues_mtg/settings.py
+++ lligues_mtg/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 import os
-import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -125,5 +124,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-django_heroku.settings(locals())
